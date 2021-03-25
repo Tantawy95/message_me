1b1b84d6f86bd30486e9c1b153d977ca66f26834
Ali Tantawy <ali.e.tantawy@gmail.com>
2021-03-25 10:02:26 +0200

    building chatroom and messages

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,8 +1,8 @@
 class ChatroomController < ApplicationController
 
 
-def index
-
-end
+  def index
+    @messages = Message.all
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username:"Ali", password: "password")
+User.create(username:"Ali2", password: "password2")
+User.create(username:"Ali3", password: "password3")
+User.create(username:"Ali4", password: "password4")
+User.create(username:"Ali5", password: "password5")
