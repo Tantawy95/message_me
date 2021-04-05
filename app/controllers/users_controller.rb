fdc2fe7d2b129fa32e7336487f99b98a6aa30c91
Ali Tantawy <ali.e.tantawy@gmail.com>
2021-04-05 12:55:54 +0200

    online when signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      @user.is_online = true
+      @user.save
       session[:user_id] = @user.id
       flash[:success] = "Welcome to MessageMe #{@user.username}"
       redirect_to root_path
